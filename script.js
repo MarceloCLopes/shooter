e403d5cd6ef4ea5e1deebc660f2bb35e8e2214e6
MarceloCLopes <marc01mcl@gmail.com>
2020-10-02 14:52:57 -0300

    Criar inimigos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const yourShip = document.querySelector(".player-shooter");
 const playArea = document.querySelector("#main-play-area");
+const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png']
 
 //Movimento e tiro da nave
 function flyAhip(event) {
@@ -69,6 +70,19 @@ function moveLaser(laser) {
       laser.style.left = `${xPosition + 8}px`;
     }
   }, 10)
+}
+
+//Criação dos inimigos aleatórios
+function createAliens() {
+  let newAlien = document.createElement("img");
+  let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)];
+  newAlien.src = alienSprite;
+  newAlien.classList.add("alien");
+  newAlien.classList.add("alien-transition");
+  newAlien.style.left = "370px";
+  newAlien.style.top = `${Math.floor(Math.random() * 330) + 30}px`;
+  playArea.appendChild(newAlien);
+  moveAlien(newAlien);
 }
 
 
